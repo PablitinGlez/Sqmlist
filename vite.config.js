2152d5ab1f92898738cfe6a16d8c08956d08344c
Juan Pablo <gonzalez0421200@gmail.com>
2025-07-23 20:36:07 -0600

    feat: Implementacion de pagina de nosotros

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        minify: true, 
+        cssMinify: true, 
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            }
+        }
+    }
 });
